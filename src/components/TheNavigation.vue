86afa7972f551396361dcc5956f2177e0e9e7a94
ChrisWMichel <christopherw.michel@gmail.com>
2024-11-17 11:42:34 -0600

    Adjusting links

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -11,7 +11,6 @@
             {{ destination.name }}          
         </app-link>
         <app-link :to="{name: 'protected'}">Dashboard</app-link>
-        <!-- <app-link to="https://vueschool.io">Vue School</app-link> -->
     </div>
 </template>
 
